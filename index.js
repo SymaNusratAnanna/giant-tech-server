c1c9ae00e0bdd397e00155444aae7a65c6b2511e
Syma Nusrat <symanusrat158@gmail.com>
2022-05-26 08:27:57 -0700

    my orders added in server site

--- index.js
+++ index.js
@@ -69,6 +69,14 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
            res.send(result);
          });
 
+         app.get('/order', async(req, res)=>{
+           const email = req.query.email;
+           const query = {email : email};
+           const orders = await orderCollection.find(query).toArray();
+           res.send(orders);
+         })
+
+
 
          app.post('/order',async(req,res)=>{
           const order=req.body;
